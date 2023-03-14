12efecf33b06665628d3a0e3bbf62a15ee4da5e7
DESKTOP-ILDHSHN\KGA_11 <wodn625@naver.com>
2023-03-14 17:39:35 +0900

    [Jaewoo] Inventory Resource & Base Add & ItemScripte

---
+++ MoonLighter/Assets/Scripts/Jaewoo/Inventory/InventorySlot.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    [SerializeField]
+    private int[,] slotArray = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
