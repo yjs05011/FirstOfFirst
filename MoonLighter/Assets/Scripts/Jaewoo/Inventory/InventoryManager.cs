2f50d158dccde11dbdf1e3c27a000c5a0cb259e0
DESKTOP-ILDHSHN\KGA_11 <wodn625@naver.com>
2023-03-17 13:36:09 +0900

    [Jaewoo] Inventory R&D

--- MoonLighter/Assets/Scripts/Jaewoo/Inventory/InventoryManager.cs
+++ MoonLighter/Assets/Scripts/Jaewoo/Inventory/InventoryManager.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
    //5x5 정수배열을 만듬
-   //정수 배열 위치마다 slot프리팹의 위치를 순서대로 받게 해줌.
-   //정수 배열 위치마다 slot값을 받게 해줌.
-   //
+   //정수 배열 위치(0,0)~(4,4마)다 slot프리팹의 위치를 순서대로 넣어줌.
+   //정수 배열 위치마다 [slot의 데이터 값]을 받게 해줌
+
+//아이템으로 태그된걸 먹었어요.
+//질문이
+//콜라이더 태그로 아이템이란걸 먹었어요
+//이 아이템은 무슨아이템인지 판단을
+   //인벤토리 매니저로 드랍된 아이템 콜라이더 충돌로 확인. 
+   //tag= = "아이템"
+
+   
+   //확인된 아이템의 이름을 판단해서 인벤토리에서 인스턴스?
    private Slot[] mSlot;
    [SerializeField]
    //슬롯 5x5 배열 
@@ -29,18 +38,27 @@ public class InventoryManager : MonoBehaviour
    //배열 자리
    private int[,] mInventoryArray = new int[5,5];
 
-   // void Start()
-   // {
-   //    for(int indexArrayY = 0; indexArrayY < mInventoryArray.Length; )
-   //    mInventoryArray
-   // }
+   
+   void Start()
+   {
+      //배열 초기화 및 선언
+      for(int indexArrayY = 0; indexArrayY < mInventoryArray.Length; indexArrayY ++)
+      {
+         for(int indexArrayX = 0; indexArrayX < mInventoryArray.Length; indexArrayX ++)
+         {
+            //mInventoryArray[indexArrayY,indexArrayX] = 
+         }
+
+      }
+      
+   }
    public void InventorySlotArray()
    {      
       for(int indexY = 0; indexY < mSlotArray.Length ; indexY ++)
       {
          for(int indexX = 0; indexX < mSlotArray.Length; indexX ++)
          {
-            //mSlotArray[indexY,indexX]
+           // mSlotArray[indexY,indexX]
          }
       }
    }
