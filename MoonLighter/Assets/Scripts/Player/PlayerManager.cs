0a47c3285170fd8b98802e042cb0f1346339e2a3
ANJAEUK <yjs05011@naver.com>
2023-03-21 12:50:54 +0900

    [Jaeuk] Make ShopPlayerPrefabs, PlayerManager

--- MoonLighter/Assets/Scripts/Player/PlayerManager.cs
+++ MoonLighter/Assets/Scripts/Player/PlayerManager.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class PlayerManager : GSingleton<PlayerManager>
 {
     //플레이어 스텟 스크립트 변수
-    public PlayerStat PlayerStat;
+    [SerializeField]
+    public PlayerStat mPlayerStat = default;
+    //현재 플레이어가 Ui를 켰는지 확인하는 함수
+    public bool mIsUiActive = default;
+    public Vector2 mPlayerBeforPos = default;
+    public bool mIsPlayerHit = default;
+    // 현재 플레이어의 위치가 상점인지 체크하는 변수
+    public bool mIsShop = default;
     protected override void Init()
     {
         base.Init();
+        mPlayerStat = new PlayerStat();
     }
 }
---
+++ MoonLighter/Assets/Scripts/Player/PlayerScriptObjs.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerStat", menuName = "PlayerStat/Obj")]
+public class PlayerScriptObjs : ScriptableObject
+{
+    public float hp;
+    public float Speed;
+    public float str;
+    public float def;
+    public float Money;
+
+
+
+}
