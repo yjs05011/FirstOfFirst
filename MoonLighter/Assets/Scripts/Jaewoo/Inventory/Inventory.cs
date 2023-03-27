4ef5e096c77e92ac29b0206439b2bdd1484c6641
Parkjaewoo39 <wodn625@naver.com>
2023-03-28 00:54:38 +0900

    [Jaewoo] Inventory move

--- MoonLighter/Assets/Scripts/Jaewoo/Inventory/Inventory.cs
+++ MoonLighter/Assets/Scripts/Jaewoo/Inventory/Inventory.cs
@@ -7,8 +7,9 @@ public class Inventory : MonoBehaviour
 {
     public const int ARRAY_X = 5;
     public const int ARRAY_Y = 4;
-    public int mSeletX = 0;
-    public int mSeletY = 0;
+    public int mSelectX = 0;
+    public int mSelectY = 0;
+    public int mKeyCodeICount = 0;
     //인벤토리 활성화 확인. true면 인벤토리가 열림.이동 막음
     public static bool mIsInventoryActiveCheck = false;
     //마을인지 확인. false면 돋보기 칸 비활성화, true면 활성화
@@ -39,8 +40,7 @@ public class Inventory : MonoBehaviour
         mInventoryArray = new GameObject[4, 5];
         mSlotValue = new GameObject[4, 5];
         
-        transform.GetChild(0).gameObject.SetActive(true);
-        
+        transform.GetChild(0).gameObject.SetActive(true);        
     }
     void Start()
     {
@@ -50,7 +50,6 @@ public class Inventory : MonoBehaviour
         mSlotEquip = mChnageEquipmentSlotGrideSetting.GetComponentsInChildren<Slot>();
         mSelectPoint =mInventoryFindSlot.transform.GetChild(1).gameObject;
        
-
         //4,5 배열 초기화 및 선언
         for (int indexY = 0; indexY < ARRAY_Y; indexY++)
         {
@@ -60,46 +59,62 @@ public class Inventory : MonoBehaviour
 
                 mInventoryArray[indexY, indexX] = mInventoryFindSlot.transform.Find("InventorySlot").GetChild(indexAdd).gameObject;
                 mInventoryArray[indexY, indexX].SetActive(true);
-                InventoryManager.Instance.mSlots.Add(mInventoryArray[indexY, indexX]);               
-               
+                InventoryManager.Instance.mSlots.Add(mInventoryArray[indexY, indexX]);   
             }
         }
-         mSelectPoint.transform.localPosition = mInventoryArray[0,0].transform.localPosition;
-        
-        
-
+        mSelectPoint.transform.localPosition = mInventoryArray[0,0].transform.localPosition;
     }
-
 
     void Update()
     {
-        TryOpenInventory();
-        Debug.Log(Input.GetKeyDown(KeyCode.D));
+          
+        TryOpenInventory();       
+        if(mIsSelectMove == true)
+        {
+
         if (Input.GetKeyDown(KeyCode.D))
         {
-            mSeletX++;
-            mSelectPoint.transform.localPosition = mInventoryArray[0,mSeletX].transform.localPosition;
-            Debug.Log(mSelectPoint.transform.position);
+            mSelectX++;
+            if( 4 < mSelectX )
+            {
+                mSelectX = 0;
+            }
+            Debug.Log(mInventoryArray[mSelectY, mSelectX]);              
+            mSelectPoint.transform.localPosition = mInventoryArray[mSelectY,mSelectX].transform.localPosition;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {                   
+            mSelectX --;
+            if(mSelectX < 0 )
+            {
+                mSelectX = 4;
+            }
+            Debug.Log(mInventoryArray[mSelectY, mSelectX]);              
+            mSelectPoint.transform.localPosition = mInventoryArray[mSelectY,mSelectX].transform.localPosition;
+            // Debug.Log(mSeletX);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {                   
+            mSelectY --;
+            if(mSelectY < 0 )
+            {
+                mSelectY = 3;
+            }                
+            Debug.Log(mInventoryArray[mSelectY, mSelectX]);             
+            mSelectPoint.transform.localPosition = mInventoryArray[mSelectY, mSelectX].transform.localPosition;   
 
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {                   
+            mSelectY ++;
+            if(3 < mSelectY )
+            {
+                mSelectY = 0;
+            }
+            Debug.Log(mInventoryArray[mSelectY, mSelectX]);            
+            mSelectPoint.transform.localPosition = mInventoryArray[mSelectY, mSelectX].transform.localPosition;     
 
-            // mSelectPoint[mSeletY, mSeletX + moveX].SetActive(true);
-            // mSelectPoint[mSeletY, mSeletX].SetActive(false);
-        }if (Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log(mInventoryArray[0,0]);
-            Debug.Log(mInventoryArray[0,1]);
-            Debug.Log(mInventoryArray[0,2]);
-
-
-            
-            mSeletX--;
-            Debug.Log(mSeletX);
-            mSelectPoint.transform.localPosition = mInventoryArray[0,mSeletX].transform.localPosition;
-            Debug.Log(mSelectPoint.transform.position);
-
-
-            // mSelectPoint[mSeletY, mSeletX + moveX].SetActive(true);
-            // mSelectPoint[mSeletY, mSeletX].SetActive(false);
+        }                      
         }
     }
 
@@ -109,47 +124,48 @@ public class Inventory : MonoBehaviour
         //KeyManager를 통해 키보드 입력(매니저에서 받는)
         //if(Input.GetKeyDown(GameKeyManger.KeySetting.keys[GameKeyManger.KeyAction.UP]))
         //화요일 머지시 지워야함(i눌러서 인벤토리 나오는 부분)
+        
         if (Input.GetKeyDown(KeyCode.I))
-        {
-
-            
+        {            
             // mSelectPoint = Instantiate(mSelectPointPrefab);
             // mSelectPoint.transform.SetParent(mInventoryFindSlot.transform.GetChild(0));
-           
-
-            mIsInventoryActiveCheck = !mIsInventoryActiveCheck;
-            if (mIsInventoryActiveCheck)
-            {
-                OpenInventory();
-
+                     mIsInventoryActiveCheck = !mIsInventoryActiveCheck;
+            if(Input.GetKeyDown(KeyCode.I))
+            {                   
+                mKeyCodeICount ++;
+                if(mKeyCodeICount ==1)
+                {                                       
+                    OpenInventory();
+                }   
+                if(mKeyCodeICount == 2)
+                {                    
+                    mKeyCodeICount = 0;
+                    CloseInventory();
+                }  
             }
-            else
-            {
-                CloseInventory();
-            }
-           
-            
+            // if (mIsInventoryActiveCheck)
+            // {
+            //     OpenInventory();
+            // }
+            // else
+            // {                
+            //     CloseInventory();
+            // }  
         }
     }
 
     //GameObject 인벤토리 베이스 활성화 
     private void OpenInventory()
-    {
+    {           
         mChangeInventoryBase.SetActive(true);   
-             
-
     }
     //GameObject 인벤토리 베이스 비활성화 
     private void CloseInventory()
-    {
+    {                     
         mChangeInventoryBase.SetActive(false);
-        mIsSelectMove = false;
+        mIsSelectMove = false;        
     }
-
    
-
-
-
     public void AcpuireItem(Item item, int itemCount)
     {
         //Debug.Log(Item.ItemE)
@@ -166,10 +182,13 @@ public class Inventory : MonoBehaviour
                         //슬롯 개수 올림
                         //mSlot[index].SetSlotCount(itemCount += itemCount);
                         mSlot[index].SetSlotCount(itemCount++);
-                        if (itemCount < 10)
+                        if ( itemCount < 10 )
                         {
                             return;
+
                         }
+                        
+                        
                     }
                 }
             }
--- MoonLighter/Assets/Scripts/Jaewoo/Inventory/InventoryManager.cs
+++ MoonLighter/Assets/Scripts/Jaewoo/Inventory/InventoryManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class InventoryManager : GSingleton<InventoryManager>
 {   
     //2차원 배열판
-    public GameObject[] mInventoryArray; 
+    public GameObject[] mInventoryArray;
     //2차원 배열을 담는
     public List<GameObject> mSlots = new List<GameObject>();
 
@@ -16,5 +16,9 @@ public class InventoryManager : GSingleton<InventoryManager>
    
    //서왑 temp 말고 C#의 기능이용
    //("선택한 아이템", "바꿔야할 아이템") = ("바꿔야할 아이템", "선택한 아이템");
+   public void Swap()
+   {
+
+   }
 
 }
